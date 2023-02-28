3df713136c493c997f5af20b9f3ae06626feb31f
ccccccccccccccccccccccccccccccccccc <30315100+ccccccccccccccccccccccccccccccccccc@users.noreply.github.com>
2023-02-28 16:32:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -36,29 +36,27 @@ const USER_CONFIG = {
         {
           type: '生日', name: '宝贝', year: '1995', date: '11-15',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2021-12-08' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: '092d194aad044c99afe52bd758b1aa54',
     }
   ],
 
