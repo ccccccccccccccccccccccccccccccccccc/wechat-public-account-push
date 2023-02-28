04b661b82c62ada379f96b6ef7feae6bc2a73220
ccccccccccccccccccccccccccccccccccc <30315100+ccccccccccccccccccccccccccccccccccc@users.noreply.github.com>
2023-02-28 16:58:12 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -24,15 +24,13 @@ const TEMPLATE_CONFIG = [
       
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      今天是我们相爱的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
       ---
       
-      {{moment_copyrighting.DATA}}
-      
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
+
     `
   },
   {
